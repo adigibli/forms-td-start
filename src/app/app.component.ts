00f5572f9e75327119979001ebb363eb9ef72930
adigibli <gibliadi@gmail.com>
2020-10-14 14:27:36 +0300

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,15 @@ export class AppComponent {
   @ViewChild('f') onlineForm: NgForm;
   defaultSecret = "pet";
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.onlineForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm){
